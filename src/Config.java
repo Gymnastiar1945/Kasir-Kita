05e058a4fcb9f9fae5e52b07e016596f841ead7f
yogan <yogan@ZEPHYRUS-G14>
2021-11-21 19:51:50 +0700

    tes eror

--- src/Config.java
+++ src/Config.java
@@ -34,7 +34,7 @@ public class Config {
            return true;
        } catch(Exception e){
            Logger.getLogger(Config.class.getName()).log(Level.SEVERE,null,e);
-           System.out.println("Koneksi gagal");
+           System.out.println("Koneksi error");
            return false;
        }
    }
